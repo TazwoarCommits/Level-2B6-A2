475d07522fdbf04f66eb30a941d696afaefb5da7
Fahim Tazwoar Sourav <tazwoarlives@gmail.com>
2025-12-06 00:34:35 +0600

    secured a connection string with DB

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
+import { Pool } from 'pg'
 
 const app = express();
 app.use(express.json());
 
 const port = 5000;
+
+const pool = new Pool({
+    connectionString : `postgresql://neondb_owner:npg_0AKtsZpPoE1U@ep-morning-sun-a8jcfv0m-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hell Yeah!");
